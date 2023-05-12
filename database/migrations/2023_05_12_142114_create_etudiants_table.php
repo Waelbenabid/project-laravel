610b2528bfee4634a9faf3594afb891fc71d8110
Waelbenabid <waelbenabid1@gmail.com>
2023-05-12 23:29:47 +0100

    test

---
+++ database/migrations/2023_05_12_142114_create_etudiants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->string('phone');
+            $table->date('date_de_naissance');
+            $table->string('Adresse_mail');
+            $table->string('Adresse_maison');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiants');
+    }
+}
